7da9add96d591295b13cc57a4c88584064e1d560
Vinogradov Vlad <vinogradvladua@gmail.com>
2023-07-26 23:03:16 +0300

    Create Loading Spinner Control

---
+++ LoadingSpinnerControl/Converters/DiameterAndThicknessToStrokeDashArrayConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace LoadingSpinnerControl.Converters;
+
+public class DiameterAndThicknessToStrokeDashArrayConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.Length < 2
+            || !double.TryParse(values[0].ToString(), out var diameter)
+            || !double.TryParse(values[1].ToString(), out var thickness))
+        {
+            return 0;
+        }
+        var circumference = Math.PI * diameter;
+        var lineLength = circumference * 0.75;
+        var gapLength = circumference - lineLength;
+        return new DoubleCollection(new[] { lineLength / thickness, gapLength / thickness });
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
